2e7bdfac6025e4a75897057ef32edafb0b2523a8
Sofiia13 <sophiyastanish@gmail.com>
2025-06-09 17:14:25 +0300

    fix bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,11 +22,17 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   const ul = document.createElement('ul');
-  Object.keys(data).forEach(key => {
+
+  Object.keys(data).forEach((key) => {
     const li = document.createElement('li');
+
     li.textContent = key;
 
-    if (typeof data[key] === 'object' && data[key] !== null) {
+    if (
+      typeof data[key] === 'object' &&
+      data[key] !== null &&
+      Object.keys(data[key]).length > 0
+    ) {
       createTree(li, data[key]);
     }
 
